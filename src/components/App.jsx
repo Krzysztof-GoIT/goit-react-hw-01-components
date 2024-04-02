433027e2ef1995ace5d813e14f32e77d76ac0c3b
Krzysztof-GoIT <145707752+Krzysztof-GoIT@users.noreply.github.com>
2024-04-02 17:05:56 +0200

    Add Statistics Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from 'components/Profile/Profile';
 import user from 'components/Profile/user.json';
+import { Statistics } from 'components/Statistics/Statistics';
+import data from 'components/Statistics/data.json';
 
 export const App = () => {
   return (
@@ -11,6 +13,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
 };
